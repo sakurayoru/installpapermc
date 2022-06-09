02a8bf7510a7a2861556d109150b2887cfd096d7
sakurayoru <hiroseshun2000@gmail.com>
2022-06-10 01:26:17 +0900

    [update]new api

--- setuppapermc.sh
+++ setuppapermc.sh
@@ -5,45 +5,45 @@ if [ "$ME" == "$USERNAME" ] ; then
     echo ""
     echo "create minecraft excute user"
     useradd -m minecraft
-    
+
     echo ""
     echo "package update"
     dnf update -y
-    
+
     echo ""
     echo "install vim java17 epel-release screen"
     dnf -y install vim wget java-17-openjdk epel-release
     dnf -y install screen
-    
+
     echo ""
     echo "open port 25565/tcp"
     firewall-cmd --add-port=25565/tcp
     firewall-cmd --runtime-to-permanent
     firewall-cmd --list-ports
-    
+
     echo ""
     echo "create minecraft use directory"
     mkdir -p /opt/mc/{server,sh}
-    
+
     echo ""
     echo "use paper base version?"
     echo "(example:1.18.1)"
     read -r PAPERVERSION
-    
+
     echo ""
     echo "search latest build"
-    BUILDNUM=$(curl https://papermc.io/api/v2/projects/paper/versions/"$PAPERVERSION" | sed 's/^\[{\(.*\)}]$/\1/' | tr ',' '\n' | tail -n1 | sed s/\]\}//)
-    
+    BUILDNUM=$(curl https://api.papermc.io/v2/projects/paper/versions/"$PAPERVERSION" | sed 's/^\[{\(.*\)}]$/\1/' | tr ',' '\n' | tail -n1 | sed s/\]\}//)
+
     echo ""
     echo "Download paper-$PAPERVERSION-$BUILDNUM"
-    wget https://papermc.io/api/v2/projects/paper/versions/"$PAPERVERSION"/builds/"$BUILDNUM"/downloads/paper-"$PAPERVERSION"-"$BUILDNUM".jar -O /opt/mc/server/paper.jar
+    wget https://api.papermc.io/v2/projects/paper/versions/"$PAPERVERSION"/builds/"$BUILDNUM"/downloads/paper-"$PAPERVERSION"-"$BUILDNUM".jar -O /opt/mc/server/paper.jar
     touch /opt/mc/server/paper-"$PAPERVERSION"-"$BUILDNUM"
-    
+
     echo ""
     echo "eula true? or false?"
     echo "type ture or false"
     read -r mceula
-    
+
     if [ "$mceula" = "true" ]; then
         echo ""
         echo "eula=true" > /opt/mc/server/eula.txt
@@ -51,7 +51,7 @@ if [ "$ME" == "$USERNAME" ] ; then
         echo ""
         echo "eula=false" > /opt/mc/server/eula.txt
     fi
-    
+
     echo ""
     echo "copy script"
     cp start.sh /opt/mc/sh/
@@ -61,11 +61,11 @@ if [ "$ME" == "$USERNAME" ] ; then
     chmod +x /opt/mc/sh/stop.sh
     chmod +x /opt/mc/sh/save.sh
     chmod +x /opt/mc/server/paper.jar
-    
+
     echo ""
     echo "change owner"
     chown -R minecraft:minecraft /opt
-    
+
     echo ""
     echo "copy systemd script"
     cp paper-restart.service /usr/lib/systemd/system/
@@ -73,7 +73,7 @@ if [ "$ME" == "$USERNAME" ] ; then
     cp paper-save.service /usr/lib/systemd/system/
     cp paper-save.timer /usr/lib/systemd/system/
     cp paper.service /usr/lib/systemd/system/
-    
+
     echo ""
     echo "enable & start systemd service"
     systemctl enable paper-restart.service
